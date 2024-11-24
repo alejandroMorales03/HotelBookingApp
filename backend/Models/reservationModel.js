b032b59f7ddf484ae47cf5fafe94406d4bfc9bc9
sof <sofritosonline@gmail.com>
2024-11-24 17:26:34 -0500

    fix

--- backend/Models/reservationModel.js
+++ backend/Models/reservationModel.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../config/db";
+import { sequelize } from "../config/db.js";
 
 const Reservations = sequelize.define('Reservations', {
     start_date:{
